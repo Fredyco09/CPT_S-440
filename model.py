2f9a4d1da6752a1e0f5cb7778dc4fca1de615bc1
Fredyco09 <fredy.corona@wsu.edu>
2026-03-09 15:16:59 -0700

    Clean

--- model.py
+++ model.py
@@ -6,8 +6,9 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 def main():
-    print("1. Loading the IMDB dataset...")
-   
+
+    # loading, slicing, and splitting
+    print("Loading the IMDB dataset...")
     dataset = load_dataset("imdb")
  
     train_data = dataset['train'][:5000]
@@ -19,15 +20,14 @@ def main():
     X_test_raw = test_data['text']
     y_test = test_data['label']
 
-    print("2. Preprocessing text and extracting features (TF-IDF)...")
+    print(" processing text and extracting features...")
     
+    # using scikit library to convert raw text
     vectorizer = TfidfVectorizer(max_features=5000, stop_words='english')
-    
-
     X_train_tfidf = vectorizer.fit_transform(X_train_raw)
     X_test_tfidf = vectorizer.transform(X_test_raw)
 
-    print("3. Training the Naive Bayes Baseline Model...")
+    print("Training the Naive Bayes Baseline Model...")
 
     nb_model = MultinomialNB()
     nb_model.fit(X_train_tfidf, y_train)
